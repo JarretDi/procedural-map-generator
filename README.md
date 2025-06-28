eaa1f57aecc8d87ab27debe0bab0e4aaa962847b
Jarret Diep <jarrettcdi@gmail.com>
2025-06-27 22:10:17 -0600

    add Base/GenLayer, define useful constants
     - certain tiles from tileset have transparent background, so base layer is necessary for visuals
     - defined atlas id and various tiles that will be generated
     - updated README with tileset source

---
+++ README.md
@@ -0,0 +1,4 @@
+# README
+
+# CREDITS:
+	Tileset by Shade from https://merchant-shade.itch.io/16x16-mini-world-sprites under Creative Commons Zero v1.0 Universal
---
+++ map.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var base_layer: TileMapLayer = $BaseLayer
+@onready var gen_layer: TileMapLayer = $GenLayer
+
+@export var MAP_SIZE : int;
+
+const ATLAS_ID = 0;
+const WATER = Vector2i(9,15);
+const SAND = Vector2i(10,16);
+const GRASS = Vector2i(10,15);
+const FOREST = Vector2i(1,15);
+const MOUNTAIN = Vector2i(5,11);
+
+func _ready() -> void:
+	for x in MAP_SIZE:
+		for y in MAP_SIZE:
+			base_layer.set_cell(Vector2i(x,y), ATLAS_ID, GRASS);
