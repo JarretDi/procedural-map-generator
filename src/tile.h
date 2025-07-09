c192c85d100b9aaf3d7f91d75c43d60c64d2b067
Jarret Diep <jarrettcdi@gmail.com>
2025-07-09 15:34:17 -0600

    change possibleTiles to use a bitset for space efficiency

--- src/tile.h
+++ src/tile.h
@@ -1,11 +1,13 @@
 #pragma once
 
-#include <set>
+#include <array>
+#include <bitset>
 #include "random_generator.hpp"
 
 #include <godot_cpp/variant/vector2i.hpp>
 
-using std::set;
+using std::bitset;
+using std::array;
 using namespace godot;
 
 class Tile {
@@ -13,19 +15,21 @@ class Tile {
         // Vector2i used for coords on map
         Vector2i coords;
 
-        // Vector2i used to get atlas coords
-        set<Vector2i> possibleTiles;
+        // represents the possible types, with each bit mapped to one type
+        // 1 represents possible, 0 represents impossible
+        bitset<32> possibleTiles;
     
     public:
 
-        Tile(const Vector2i & coords, const set<Vector2i> & possibleTiles);
+        Tile(const Vector2i & coords, int typeCount);
 
         int getPriority() const;
 
-        void removeTypesNotIn(const set<Vector2i> & types);
+        void removeTypesNotIn(const bitset<32> & types);
 
         Vector2i getCoords() const;
 
-        // picks a random tileType among its options, and then returns it
-        Vector2i collapseTile();
+        // picks a random tileType among its options in given direction
+        // then returns the index of the bit
+        int collapseTile();
 };
