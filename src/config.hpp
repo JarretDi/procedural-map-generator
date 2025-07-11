2518733b46a8edbbf7ad59248e19b88316ed1194
Jarret Diep <jarrettcdi@gmail.com>
2025-07-10 21:43:52 -0600

    define more useful constants in config

--- src/config.hpp
+++ src/config.hpp
@@ -1,3 +1,15 @@
 #pragma once
 
+#include <array>
+#include <godot_cpp/variant/vector2i.hpp>
+
 constexpr size_t TILE_TYPE_COUNT = 64;
+
+const Vector2i NULLVEC = Vector2i(-1, -1);
+
+const std::array<Vector2i, 4> CARDINAL_VECTORS = {
+    Vector2i( 0, -1),
+    Vector2i( 1,  0),
+    Vector2i( 0,  1),
+    Vector2i(-1,  0),
+};
