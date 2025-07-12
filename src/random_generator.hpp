38d000c44e838d6f61b730798e3292e922314853
Jarret Diep <jarrettcdi@gmail.com>
2025-07-11 18:05:41 -0600

    refactor RandomGenerator, add discrete distribution functino

--- src/random_generator.hpp
+++ src/random_generator.hpp
@@ -1,11 +1,19 @@
 #pragma once
 #include <random>
+#include <vector>
 
 class RandomGenerator {
-public:
-    static int getInt(int min, int max) {
-        static std::mt19937 gen(std::random_device{}());
-        std::uniform_int_distribution<int> dist(min, max);
-        return dist(gen);
-    }
+    private:
+        inline static std::random_device rd;
+        inline static std::mt19937 gen{rd()};
+    public:
+        static int getInt(int min, int max) {
+            std::uniform_int_distribution<int> dist(min, max);
+            return dist(gen);
+        }
+
+        static int getIntDistribution(std::vector<int> weights) {
+            std::discrete_distribution<int> dist(weights.begin(), weights.end());
+            return dist(gen);
+        }
 };
